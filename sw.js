a8c0a8297109924f39a4b15042154e988fd7d327
Cappytano <63429711+Cappytano@users.noreply.github.com>
2025-08-11 16:15:52 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,10 +2,9 @@ const CACHE = 'qr-logger-v5-1';
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', (event) => {
-  const req = event.request;
-  if (req.mode === 'navigate') {
-    event.respondWith(fetch(req).catch(() => caches.match('/index.html')));
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('index.html')));
     return;
   }
-  event.respondWith(fetch(req).catch(() => caches.match(req)));
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
